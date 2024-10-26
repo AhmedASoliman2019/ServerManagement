94db5b04998c3ffb40efbd68ca9084dce1c75ece
Ahmed Soliman <DevAhmedASoliman@gmail.com>
2024-10-26 15:22:07 +0300

    .

--- ServerManagement/Models/Server.cs
+++ ServerManagement/Models/Server.cs
@@ -1,18 +1,19 @@
-﻿namespace ServerManagement.Models
+﻿using ServerManagement.Components;
+namespace ServerManagement.Models
 {
     public class Server
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? City { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
         public bool IsOnline { get; set; }
 
         public Server()
         {
-            Random rnd = new Random();
-            int RandomNumber = rnd.Next(0, 2);
-            IsOnline = RandomNumber == 0 ? false : true;
+            Random random = new Random();
+            int r = random.Next(0, 2);
+            IsOnline = r == 0 ? false : true;
         }
-    }
 
+    }
 }
