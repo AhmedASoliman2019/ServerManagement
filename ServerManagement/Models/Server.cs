836d9c4f7a63fbe84038683bb6f52cbb5988a443
Ahmed Soliman <DevAhmedASoliman@gmail.com>
2024-10-26 13:04:39 +0300

    Add project files.

---
+++ ServerManagement/Models/Server.cs
@@ -0,0 +1,18 @@
+﻿namespace ServerManagement.Models
+{
+    public class Server
+    {
+        public int ServerId { get; set; }
+        public string? ServerName { get; set; }
+        public string? ServerCity { get; set; }
+        public bool IsOnline { get; set; }
+
+        public Server()
+        {
+            Random rnd = new Random();
+            int RandomNumber = rnd.Next(0, 2);
+            IsOnline = RandomNumber == 0 ? false : true;
+        }
+    }
+
+}
